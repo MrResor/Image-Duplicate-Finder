b52168a6569dbb0d7b8b81636dbf1c67ed238e8e
Mr.Resor <jakusienko@gmail.com>
2024-06-27 22:17:43 +0200

    format files

--- auxiliary.py
+++ auxiliary.py
@@ -2,6 +2,7 @@ from __init__ import Qtc
 import time
 from os import walk
 
+
 class Get_paths(Qtc.QThread):
     """
     Runs a counter thread.
@@ -19,9 +20,8 @@ class Get_paths(Qtc.QThread):
         ext = ('.png', '.jpg', '.jpeg', '.raw', '.tiff', '.JPG')
         for root, _, files in walk(self.paths):
             self.relevant += [root + '/' * (root != './') +
-                         x for x in files if x.endswith(ext)]
+                              x for x in files if x.endswith(ext)]
             self.direc_done.emit(root)
             if not self.deep_search:
                 break
         self.search_done.emit()
-        
